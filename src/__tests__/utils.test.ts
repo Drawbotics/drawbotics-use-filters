bc260717e74d12c55d3f1c62c5e8000c98c7791f
LeopoldArkham <leopold.arkham@gmail.com>
2020-04-27 09:54:57 +0200

    update tests

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,16 +1,15 @@
-import { History, Location } from 'history';
+// @ts-nocheck
+import { Location } from 'history';
 
 import { readUrl, setUrlValue } from '../utils';
 
 describe('drawbotics-use-filter/utils.ts', () => {
   describe('readUrl', () => {
     it('reads the values in the url and return them with the right name', () => {
-      const history = {
-        location: {
-          search: '?filter[filterKey]=filterValue&filter[filterKey2]=filterValue1,filterValue2',
-        },
-      } as History;
-      const result = readUrl(history);
+      const location = {
+        search: '?filter[filterKey]=filterValue&filter[filterKey2]=filterValue1,filterValue2',
+      } as Location;
+      const result = readUrl(location);
       expect(result).toMatchObject({
         filterKey: 'filterValue',
         filterKey2: ['filterValue1', 'filterValue2'],
@@ -20,76 +19,61 @@ describe('drawbotics-use-filter/utils.ts', () => {
 
   describe('setUrlValue', () => {
     it('writes the value to the url with the right name', () => {
-      const pushSpy = jest.fn();
-      const history = {
-        location: { search: '' },
-        push(location: Location) {
-          pushSpy(location);
-        },
-      } as History;
+      const navigateSpy = jest.fn();
+      const location = {
+        search: '',
+      };
 
-      setUrlValue(history, 'filterKey', 'filterValue');
-      expect(pushSpy.mock.calls[0][0].search).toEqual('filter[filterKey]=filterValue');
+      setUrlValue(location, navigateSpy, 'filterKey', 'filterValue');
+      expect(navigateSpy.mock.calls[0][0].search).toEqual('filter[filterKey]=filterValue');
     });
 
     it('handle correctly array values', () => {
-      const pushSpy = jest.fn();
-      const history = {
-        location: { search: '' },
-        push(location: Location) {
-          pushSpy(location);
-        },
-      } as History;
+      const navigateSpy = jest.fn();
+      const location = {
+        search: '',
+      };
 
-      setUrlValue(history, 'filterKey', ['filterValue1', 'filterValue2']);
+      setUrlValue(location, navigateSpy, 'filterKey', ['filterValue1', 'filterValue2']);
 
-      expect(pushSpy.mock.calls[0][0].search).toEqual(
+      expect(navigateSpy.mock.calls[0][0].search).toEqual(
         'filter[filterKey]=filterValue1,filterValue2',
       );
     });
 
     it('does not delete existing values', () => {
-      const pushSpy = jest.fn();
-      const history = {
-        location: { search: '?filter[filterKey]=filterValue1,filterValue2' },
-        push(location: Location) {
-          pushSpy(location);
-        },
-      } as History;
+      const navigateSpy = jest.fn();
+      const location = {
+        search: '?filter[filterKey]=filterValue1,filterValue2',
+      };
 
-      setUrlValue(history, 'filterKey2', 'filterValue3');
+      setUrlValue(location, navigateSpy, 'filterKey2', 'filterValue3');
 
-      expect(pushSpy.mock.calls[0][0].search).toEqual(
+      expect(navigateSpy.mock.calls[0][0].search).toEqual(
         'filter[filterKey2]=filterValue3&filter[filterKey]=filterValue1,filterValue2',
       );
     });
 
     it('updates correctly an existing value', () => {
-      const pushSpy = jest.fn();
-      const history = {
-        location: { search: '?filter[filterKey]=filterValue' },
-        push(location: Location) {
-          pushSpy(location);
-        },
-      } as History;
+      const navigateSpy = jest.fn();
+      const location = {
+        search: '?filter[filterKey]=filterValue',
+      };
 
-      setUrlValue(history, 'filterKey', 'filterValue2');
+      setUrlValue(location, navigateSpy, 'filterKey', 'filterValue2');
 
-      expect(pushSpy.mock.calls[0][0].search).toEqual('filter[filterKey]=filterValue2');
+      expect(navigateSpy.mock.calls[0][0].search).toEqual('filter[filterKey]=filterValue2');
     });
 
     it('does not update the query string if the new value is equal to the current one', () => {
-      const pushSpy = jest.fn();
-      const history = {
-        location: { search: '?filter[filterKey]=filterValue' },
-        push(location: Location) {
-          pushSpy(location);
-        },
-      } as History;
+      const navigateSpy = jest.fn();
+      const location = {
+        search: '?filter[filterKey]=filterValue',
+      };
 
-      setUrlValue(history, 'filterKey', 'filterValue');
+      setUrlValue(location, navigateSpy, 'filterKey', 'filterValue');
 
-      expect(pushSpy.mock.calls.length).toEqual(0);
+      expect(navigateSpy.mock.calls.length).toEqual(0);
     });
   });
 });
